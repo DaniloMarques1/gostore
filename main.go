8d0d1ab2b6a56a21387230b40f83a6af78a2b748
Danilo Marques <danilomarques20@hotmail.com>
2021-10-08 21:56:31 -0300

    testing for invalid syntax and adding tests.

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"errors"
 	"log"
 	"net"
 	"strings"
@@ -12,8 +13,61 @@ const (
 	ADDRESS = ":5000"
 )
 
+// operations supported
+const (
+	OP_STORE  = "store"
+	OP_DELETE = "delete"
+	OP_READ   = "read"
+)
+
+// error messages
+const (
+	InvalidSyntax         = "Message is not valid. Invalid syntax"
+	OperationNotSupported = "Operation is not supported"
+)
+
 type Store struct {
 	db map[string]interface{}
+}
+
+type Message struct {
+	op    string
+	key   string
+	value interface{}
+}
+
+type Response struct {
+	code int
+	msg  string
+}
+
+type Operation interface {
+	executeOperation() error
+}
+
+type StoreOperation struct {
+	key   string
+	value interface{}
+}
+
+type ReadOperation struct {
+	key string
+}
+
+type DeleteOperation struct {
+	key string
+}
+
+func (sop StoreOperation) executeOperation() error {
+	return nil
+}
+
+func (dop DeleteOperation) executeOperation() error {
+	return nil
+}
+
+func (rop ReadOperation) executeOperation() error {
+	return nil
 }
 
 func main() {
@@ -34,29 +88,97 @@ func main() {
 	}
 }
 
+// handle the new connection
 func handleConnection(conn net.Conn) {
 	log.Printf("Handling connection\n")
-	//defer conn.Close()
+	for {
+		//defer conn.Close()
 
-	msg, err := bufio.NewReader(conn).ReadString('\n')
-	if err != nil {
-		log.Printf("ERR: Error reading message from connection %v\n", err)
-		return
+		msg, err := bufio.NewReader(conn).ReadString('\n')
+		if err != nil {
+			log.Printf("ERR: Error reading message from connection %v\n", err)
+			return
+		}
+
+		message, err := parseMessage(msg)
+		if err != nil {
+			//TODO
+		}
+		op, err := getOperationFromMessage(message)
+		if err != nil {
+			// TODO handl err
+		}
+		op.executeOperation()
 	}
-
-	handleMessage(msg)
 }
 
 // parses the message to extract its key and value
-// message will be something like key=swee;value=1234\n
-func handleMessage(msg string) {
-	msgWithoutNewLine := strings.Replace(msg, "\n", "", -1)
+// message will be something like op=store;key=swee;value=1234\n
+func parseMessage(msg string) (*Message, error) {
+	msg = strings.Replace(msg, "\n", "", -1)
+	splited := strings.Split(msg, ";")
+		
+	if len(splited) < 2 {
+		log.Printf("Invalid syntax. Wrong number of ;\n")
+		return nil, errors.New(InvalidSyntax)
+	}
 
-	splited := strings.Split(msgWithoutNewLine, ";")
-	keySplit := strings.Split(splited[0], "=")
+	opSplit := strings.Split(splited[0], "=")
+	if len(opSplit) < 2 {
+		log.Printf("Invalid syntax on operation\n")
+		return nil, errors.New(InvalidSyntax)
+	}
+	op := opSplit[1]
+
+	keySplit := strings.Split(splited[1], "=")
+	if len(keySplit) < 2 {
+		log.Printf("Invalid syntax on key\n")
+		return nil, errors.New(InvalidSyntax)
+	}
 	key := keySplit[1]
-	valueSplit := strings.Split(splited[1], "=")
-	value := valueSplit[1]
 
-	log.Printf("\nKEY = %v\nVALUE = %v\n", key, value)
+	var value interface{}
+	if op == OP_STORE {
+		if len(splited) < 3 {
+			log.Printf("Invalid syntax on value. You did not provide a value key-value pair\n")
+			return nil, errors.New(InvalidSyntax)
+		}
+		valueSplit := strings.Split(splited[2], "=")
+		log.Printf("Value split = %v\n", valueSplit)
+		if len(valueSplit) < 2 {
+			log.Printf("Invalid syntax on value.\n")
+			return nil, errors.New(InvalidSyntax)
+		}
+		value = valueSplit[1]
+	}
+
+	return &Message{
+		op:    op,
+		key:   key,
+		value: value,
+	}, nil
+}
+
+// return the Operation that will be executed
+// based on the received message
+func getOperationFromMessage(message *Message) (Operation, error) {
+	var operation Operation
+	if message.op == OP_STORE {
+		operation = StoreOperation{
+			key:   message.key,
+			value: message.value,
+		}
+	} else if message.op == OP_DELETE {
+		operation = DeleteOperation{
+			key: message.key,
+		}
+	} else if message.op == OP_READ {
+		operation = ReadOperation{
+			key: message.key,
+		}
+	} else {
+		return nil, errors.New(OperationNotSupported) 
+	}
+
+	return operation, nil
 }
---
+++ main_test.go
@@ -0,0 +1,99 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestParseMessage(t *testing.T) {
+	msg := "op=store;key=name;value=Danilo"
+	message, err := parseMessage(msg)
+	assertNil(t, err)
+	assertEqual(t, message.key, "name")
+	assertEqual(t, message.value, "Danilo")
+	assertEqual(t, message.op, "store")
+}
+
+func TestParseMessageError1(t *testing.T) {
+	msg := "op=store,key=value"
+	_, err := parseMessage(msg)
+	assertNotNil(t, err)
+	assertEqual(t, InvalidSyntax, err.Error())
+}
+
+func TestParseMessageError2(t *testing.T) {
+	msg := "daniliisnice"
+	_, err := parseMessage(msg)
+	assertNotNil(t, err)
+	assertEqual(t, InvalidSyntax, err.Error())
+}
+
+func TestParseMessageError3(t *testing.T) {
+	msg := "da;ni;li=isnice"
+	_, err := parseMessage(msg)
+	assertNotNil(t, err)
+	assertEqual(t, InvalidSyntax, err.Error())
+}
+
+func TestParseMessageError4(t *testing.T) {
+	msg := "op=delete;ni;li=isnice"
+	_, err := parseMessage(msg)
+	assertNotNil(t, err)
+	assertEqual(t, InvalidSyntax, err.Error())
+}
+
+func TestParseMessageError5(t *testing.T) {
+	msg := "op=store;key=name;value"
+	_, err := parseMessage(msg)
+	assertNotNil(t, err)
+	assertEqual(t, InvalidSyntax, err.Error())
+}
+
+func TestGetOpFromMessage(t *testing.T) {
+	msg := Message{
+		op:    OP_STORE,
+		key:   "name",
+		value: "Danilo",
+	}
+
+	op, err := getOperationFromMessage(&msg)
+	assertNil(t, err)
+	opType, ok := op.(StoreOperation) // get the concrete type
+	assertTrue(t, ok)
+	assertEqual(t, opType.value, "Danilo")
+}
+
+func TestGetOpFromMessageError(t *testing.T) {
+	msg := Message{
+		op:    "sum", // not supported op
+		key:   "name",
+		value: "Danilo",
+	}
+
+	_, err := getOperationFromMessage(&msg)
+	assertEqual(t, OperationNotSupported, err.Error())
+}
+
+func assertEqual(t *testing.T, expected, actual interface{}) {
+	if expected != actual {
+		t.Fatalf("\nAssert equals error\n\tExpected %v\n\tActual %v\n", expected, actual)
+	}
+}
+
+func assertTrue(t *testing.T, value bool) {
+	if !value {
+		t.Fatalf("\nAssert true error\n\tgot %v\n", value)
+	}
+}
+
+// only work for err type so far
+func assertNil(t *testing.T, err interface{}) {
+	if err != nil {
+		t.Fatalf("\nAssert nil error\n\tgot %v\n", err)
+	}
+}
+
+func assertNotNil(t *testing.T, err interface{}) {
+	if err == nil {
+		t.Fatalf("\nAssert not nil error\n\tgot %v\n", err)
+	}
+}
